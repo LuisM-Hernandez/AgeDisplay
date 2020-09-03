c53de3a39b5f01cc8cc9ce75d03d584e78fab0e0
Luis <m.luishernandez@outlook.com>
2020-09-03 03:02:39 -0400

    initial commit

---
+++ AgeDisplay.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace AgeDisplay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello, may I ask your name?");
+            string user = Console.ReadLine();
+
+            Console.WriteLine("Please enter your birth year");
+            int year = Convert.ToInt32(Console.ReadLine());
+
+            var age = DateTime.Now.Year - year;
+
+            Console.WriteLine("Hello " + user + " you are " + age + " old");
+
+        }
+    }
+}
